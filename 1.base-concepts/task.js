335ee643e6001fe5ff42d994019a2c15895f995c
Julia D <batranina@gmail.com>
2022-02-06 21:39:56 +0100

    1 commit

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,13 +1,32 @@
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
+  "use strict";
+  let arr = [], root1, root2;
+  let discriminant = (-b)**2 - (4*a*c);
+  if (discriminant > 0) {
+    root1 = ((-b) + Math.sqrt(discriminant)) / (2*a);
+    root2 = ((-b) - Math.sqrt(discriminant)) / (2*a);
+    arr = [root1, root2];
+    }
+    else if (discriminant === 0) {
+     root1 = (-b)/(2*a);
+     arr = [root1];
+   };
   return arr; // array
 }
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
-  let totalAmount;
-
-  // код для задачи №2 писать здесь
-
+  "use strict";
+  let totalAmount, credit, p, creditMonths, paymentMonth;
+  if (!isNaN(percent)) {percent = Number(percent);}
+  else {return (`Параметр "Процентная ставка" содержит неправильное значение "${percent}"`)};
+  if (!isNaN(contribution)) {contribution = Number(contribution);}
+  else {return (`Параметр "Начальный взнос" содержит неправильное значение "${contribution}"`)};
+  if (!isNaN(amount)) {amount = Number(amount);}
+  else {return (`Параметр "Общая стоимость" содержит неправильное значение "${amount}"`)};
+  credit = amount - contribution;
+  p = percent/12/100;
+  creditMonths = Math.ceil((date - new Date()) / 2678400000);
+  paymentMonth = credit * (p + (p / (((1 + p)**creditMonths) - 1)))
+  totalAmount = Number((paymentMonth * creditMonths).toFixed(2));
   return totalAmount;
 }
