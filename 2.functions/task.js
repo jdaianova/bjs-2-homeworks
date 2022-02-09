83d19ace20e6919ba83b5d7ec4d2c2e17ba8ad52
Julia D <batranina@gmail.com>
2022-02-09 13:55:06 +0100

    1 commit

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -2,30 +2,54 @@
 function getArrayParams(arr) {
   let min, max, sum, avg;
 
-  // Ваш код
+  min = arr[0];
+  max = arr[0];
+  sum = arr[0];
+  for (let i = 1; i < arr.length; i++) {
+    sum = sum + arr[i];
+    if (arr[i] < min) {
+      min = arr[i]
+    };
+    if (arr[i] > max) {
+      max = arr[i]
+    };
+  };
+  avg = Number((sum/arr.length).toFixed(2));
 
   return { min: min, max: max, avg: avg };
 }
 
 // Задание 2
 function worker(arr) {
-  let sum;
-
-  // Ваш код
-
+  let sum = arr[0];
+  for (let i = 1; i < arr.length; i++) {
+    sum = sum + arr[i];
+    }
   return sum;
 }
 
 function makeWork(arrOfArr, func) {
   let max;
-
-  // Ваш кода
-  // for ...
-  
+  max = func(arrOfArr[0]);
+  for (let i = 1; i < arrOfArr.length; i++) {
+    if (max < func(arrOfArr[i])) {
+      max = func(arrOfArr[i])
+    };
+  };
   return max;
 }
 
 // Задание 3
 function worker2(arr) {
-  // Ваш код
+  let min = arr[0], max = arr[0], difference;
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] < min) {
+      min = arr[i]
+    };
+    if (arr[i] > max) {
+      max = arr[i]
+    };
+  };
+  difference = Math.abs(max-min);
+  return difference;
 }
