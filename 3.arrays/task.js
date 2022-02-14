6c10d579106c7aa1350f8f0aef5d371ed6174bfe
Julia D <batranina@gmail.com>
2022-02-14 19:00:42 +0100

    1

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,11 @@
 function compareArrays(arr1, arr2) {
   let result;
 
-  // Ваш код
+  if (arr1.length === arr2.length && arr1.every((n, i) => n === arr2[i])) {
+    result = true;
+  } else {
+    result = false;
+  };
 
   return result; // boolean
 }
@@ -9,7 +13,7 @@ function compareArrays(arr1, arr2) {
 function advancedFilter(arr) {
   let resultArr;
 
-  // Ваш код
+  resultArr = arr.filter(i => i > 0).filter(i => i % 3 === 0).map(i => i * 10);
 
   return resultArr; // array
 }
