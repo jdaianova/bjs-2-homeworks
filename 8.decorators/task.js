56575b350a152aa76e291f041d02b9aa9e102c76
Julia D <batranina@gmail.com>
2022-03-04 19:45:13 +0100

    1 commit

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,12 +1,40 @@
 function cachingDecoratorNew(func) {
-  // Ваш код
+let cache = [];
+function wrapper(...args) {
+    const hash = args.join(',');
+    let idx = cache.findIndex((item)=> item.hash === hash);
+    if (idx !== -1 ) {
+        console.log("Из кэша: " + cache[idx].value);
+        return "Из кэша: " + cache[idx].value;
+    }
+    let result = func(...args);
+    cache.push({hash: hash, value: result });
+    if (cache.length > 5) {
+      cache.splice(0,1);
+    }
+    console.log("Вычисляем: " + result);
+    return "Вычисляем: " + result;
+}
+return wrapper;
 }
 
-
-function debounceDecoratorNew(func) {
-  // Ваш код
+function debounceDecoratorNew(func, ms) {
+  let timeout, flag = false;
+  function wrapper(...args) {
+    if (flag === false) {
+      timeout = setTimeout(() => {
+        flag = false;
+        func(...args);
+      }, ms);
+      flag = true;
+    } else {
+      clearTimeout(timeout);
+      timeout = setTimeout(() => func(...args), ms);
+    }
+  }
+  return wrapper;
 }
 
-function debounceDecorator2(func) {
-  // Ваш код
+function debounceDecorator2(func,ms) {
+
 }
