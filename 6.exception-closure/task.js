9c92942e3bf3ff861216a342b90647a04588aa11
Julia D <batranina@gmail.com>
2022-03-15 17:34:02 +0100

    2 commit

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -19,6 +19,9 @@ class Triangle {
     this.side1 = side1;
     this.side2 = side2;
     this.side3 = side3;
+    if ((side1 + side2 < side3) || (side2 + side3 < side1) || (side3 + side1 < side2)) {
+      throw new Error("Треугольник с такими сторонами не существует");
+    };
   }
 
   getPerimeter() {
@@ -35,10 +38,7 @@ function getTriangle(side1, side2, side3) {
   try {
     return new Triangle(side1, side2, side3);
   } catch (e) {
-    return {getPerimeter(), getArea()};
-  };
-}
-
-if ((this.side1 + this.side2 < this.side3) || (this.side2 + this.side3 < this.side1) || (this.side3 + this.side1 < this.side2)) {
-  throw new Error("Треугольник с такими сторонами не существует");
-};
+    console.log('Ошибка! Треугольник не существует');
+    return;
+    };
+  }
