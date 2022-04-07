53390b756d1742cd4fcc2c52ae1cb1f0e4435ba6
Julia D <batranina@gmail.com>
2022-04-07 19:34:05 +0200

    3 commit

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -15,6 +15,7 @@ function validateCount(value) {
 }
 
 class Triangle {
+
   constructor(side1, side2, side3) {
     this.side1 = side1;
     this.side2 = side2;
@@ -25,20 +26,29 @@ class Triangle {
   }
 
   getPerimeter() {
+    if (typeof this == "undefined") {
+      return 'Ошибка! Треугольник не существует';
+    };
     return Number((this.side1 + this.side2 + this.side3).toFixed(3));
   }
 
   getArea() {
+    if (typeof this == "undefined") {
+      return 'Ошибка! Треугольник не существует';
+    };
     let p = this.getPerimeter() * 0.5;
     return Number(Math.sqrt(p * (p-this.side1) * (p-this.side2) * (p-this.side3)).toFixed(3));
   }
+
 }
 
 function getTriangle(side1, side2, side3) {
   try {
     return new Triangle(side1, side2, side3);
-  } catch (e) {
-    console.log('Ошибка! Треугольник не существует');
-    return;
+  } catch (error) {
+    return {
+      getPerimeter: this.getPerimeter(),
+      getArea: this.getArea()
     };
-  }
+  };
+}
